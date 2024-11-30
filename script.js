ebfdfafa9843fdc69233136ea307fa323dc5b2bf
mvinza34 <89809703+mvinza34@users.noreply.github.com>
2024-11-30 18:42:57 -0500

    Update script.js with new getHumanChoice function and score variables
    - Added new getHumanChoice function that lets the player pick either rock, paper, or scissors
    - Added new humanScore and computerScore variables to keep track of scores
    - Improves game functionality by allowing player interaction as well as keeping score

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// the CPU chooses whether to use rock, paper, or scissors
+// Keeps track of both the player and computer scores
+let humanScore = 0;
+let computerScore = 0;
+
+// Retrieves the computer's input on whether rock, paper, or scissors was chosen
 function getComputerChoice() {
     // picks a random number between 0 and 2
     let comChoice = Math.floor(Math.random() * 3);
@@ -14,4 +18,24 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+// Retrieves the player's input on whether rock, paper, or scissors was chosen
+function getHumanChoice() {
+    let choice = prompt("Choose either rock, paper, or scissors: ");
+
+    if (choice === "Rock") {
+        console.log(`You chose ${choice}.`);
+    }
+    else if (choice === "Paper") {
+        console.log(`You chose ${choice}.`);
+    }
+    else if (choice === "Scissors") {
+        console.log(`You chose ${choice}.`);
+    }
+    else {
+        console.log("That's not a valid choice.");
+    }
+
+    return choice;
+}
+
+console.log(getHumanChoice());
