110d5e2d8d0005a00b1ae81aaee56786bb259598
mvinza34 <89809703+mvinza34@users.noreply.github.com>
2024-11-30 17:44:22 -0500

    Add getComputerChoice function
    - Added getComputerChoice function that lets the computer pick either rock, paper, or scissors
    - Improves game functonality by allowing the computer to interact

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+// the CPU chooses whether to use rock, paper, or scissors
+function getComputerChoice() {
+    // picks a random number between 0 and 2
+    let comChoice = Math.floor(Math.random() * 3);
+
+    if (comChoice === 1) {
+        return "Rock";
+    }
+    else if (comChoice === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
